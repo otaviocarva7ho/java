513a665e68a406b91de92447c2a2b0de1ddb2072
Otávio Carvalho <carvalhom7otavioh@gmail.com>
2025-11-17 20:52:15 -0300

    Comentários

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,7 +1,24 @@
 package fundamentos;
+/**
+ * 
+ * @author Otávio Carvalho
+ * @since JDK1.0
+ */
 public class PrimeiroPrograma {
+    /**
+     * ...
+     * @param args
+     */
     public static void main(String[] args) {
+        // Sentença de código termina com ;
+        // Mais uma linha ...
+        // Fim
         System.out.println("Primeiro Programa Parte #01!");
+        /*
+         * Linha 01
+         * Linha 02
+         * Linha 03
+         */
         System.out.println("Primeiro Programa Parte #02!");
     }
 }
