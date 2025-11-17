9dad592b5d12ced160dabca4157401cf41e3823c
Otávio Carvalho <carvalhom7otavioh@gmail.com>
2025-11-17 19:53:10 -0300

    Primeiro Programa #02

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,6 +1,7 @@
 package fundamentos;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("Primeiro Programa Java!!!");
+        System.out.println("Primeiro Programa Parte #01!");
+        System.out.println("Primeiro Programa Parte #02!");
     }
 }
